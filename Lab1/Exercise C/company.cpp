3752b39e4b8de593b9bdf9c9dcd6ea7ff2e480e9
lordtrev4 <trevorhuan@gmail.com>
2024-09-13 10:47:09 -0600

    www

---
+++ Lab1/Exercise C/company.cpp
@@ -0,0 +1,24 @@
+/*
+*   File Name: company.cpp
+*   Assignment: Lab 1 Exercise C
+*   Completed by: Trevor Nguyen and Zachary Lam
+*   Submission Date: Sept 13, 2024 
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Comapany {
+    string companyName;
+    string companyAddress;
+
+    vector<string> employees;
+
+    string dateEstablished;
+
+    vector<string> employeeState;
+
+    vector<string> customers;
+
+};
